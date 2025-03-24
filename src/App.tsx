1a21536cef012c1d1afd50a15b94347c03114a85
nuelo <akalo.emmanuel18@gmail.com>
2025-03-24 11:13:20 +0100

    feat: implement reset password screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import SignupPage from "./components/SignupPage";
+import ResetPasswordPage from "./components/ResetPasswordPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
         {/* Add other routes as needed */}
         <Route path="/" element={<Dashboard />} /> {/* For demo purposes */}
       </Routes>
